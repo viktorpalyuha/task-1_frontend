aa391c371f89e4a4f4aad387e092ba056257fb90
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-02-18 20:20:45 +0200

    data based grid added

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	"extends": "eslint:recommended",
+	"env": {
+		"browser": true,
+		"es6": true
+	},
+	"rules": {
+		"no-console": "off",
+		"curly": "error",
+		"eqeqeq": ["error", "always"],
+		"guard-for-in": "warn",
+		"no-caller": "error",
+		"no-empty-function": "error",
+		"no-eval": "error",
+		"no-extra-bind": "error",
+		"no-multi-spaces": "error",
+		"no-new": "warn",
+		"block-spacing": "error",
+		"brace-style": "error",
+		"comma-dangle": ["error", "never"],
+		"func-call-spacing": ["error", "never"],
+		"max-len": ["error", { "code": 120, "ignoreComments": true }],
+		"new-cap": ["error", { "newIsCap": true }],
+		"new-parens": "error",
+		"no-unneeded-ternary": "error",
+		"quotes": ["warn", "single", { "avoidEscape": true , "allowTemplateLiterals": true }],
+		"arrow-spacing": ["error", { "before": true, "after": true }],
+		"no-var": "warn",
+		"no-unused-vars": ["error", { "vars": "local" }]
+	},
+	"parserOptions": {
+		"ecmaVersion": 8
+	}
+}
