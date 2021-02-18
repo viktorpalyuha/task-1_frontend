1a43582ebc51c8d97b2610d4d05bca4a693bd286
Viktor Palyuha <viktorpalyuha@gmail.com>
2021-02-18 20:23:32 +0200

    eslint checked

--- js/index.js
+++ js/index.js
@@ -10,24 +10,24 @@ async function fetchDataFromBack() {
 async function drawGrid() {
   const fetchedData = await fetchDataFromBack();
   const header = document.createElement('h1');
-  header.innerHTML = 'Car Exhibtion'
-  
+  header.innerHTML = 'Car Exhibtion';
+
   const cardsContainer = document.createElement('div');
   cardsContainer.classList.add('cards-container');
-  
+
   fetchedData.cars.map((car) => {
-      const newCar = document.createElement('div');
-      newCar.classList.add('card');
-      newCar.innerHTML = `
+    const newCar = document.createElement('div');
+    newCar.classList.add('card');
+    newCar.innerHTML = `
       <div class="container">
       <h4><b>Brand: ${car.make_display}</b></h4>
       <p>Car id: ${car.make_id}</p>
       <p>Uniqueness: ${car.make_is_common}</p>
       <p>Made in: ${car.make_country}</p>
       </div>`;
-      cardsContainer.append(newCar);
-    });
-    
+    cardsContainer.append(newCar);
+  });
+
   root.append(header);
   root.append(cardsContainer);
 }
